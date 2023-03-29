1478c064bda130a98f101a43524058f1f9828004
aajinkya1203 <aajinkya1203@gmail.com>
2023-03-30 01:29:07 +0530

    Image added

--- src/Components/logo.js
+++ src/Components/logo.js
@@ -5,13 +5,14 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import { AccountCircle } from '@mui/icons-material';
-
+import LOGO from '../Images/logo.jpg'
 export default function Logo() {
 
   return (
     <AppBar position="static" sx={{ background: "#5a189a" }} >
       <Box sx={{ flexGrow: 1 }}>
         <Toolbar>
+          <img src={LOGO} width={"auto"} height={"50px"} style={{ margin: "10px", boxSizing: "border-box" }} />
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Recruit Mate
           </Typography>
