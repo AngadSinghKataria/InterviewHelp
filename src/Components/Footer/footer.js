a000fa5540627d122ef3c9053554515c0d91a2eb
aajinkya1203 <aajinkya1203@gmail.com>
2023-03-31 19:01:08 +0530

    Removed footer thing

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -1,20 +1,19 @@
 import Logo from "../logo";
 import styles from './footer.module.css'
 import Text from '../text.js'
+import { Box } from "@mui/material";
 
 function Footer() {
   var marginText = '0px 0.88vw'
   var marginTextBold = '0px 0.88vw 1.7391vh'
   return (
     <div className={styles.container}>
-        <div style={{alignContent: "center", display: "flex", justifyContent: "center", padding: '10px'}}>
-            <Logo color='white'/>
-            <Text data="Copyright. NMIMS Job Portal 2023."/>
-            <Text data="All Rights Reserved." />
-        </div>
+      <div style={{ alignContent: "center", display: "flex", justifyContent: "space-evenly", padding: '10px', width: "100%" }}>
+        <Text data="Copyright. NMIMS Job Portal 2023." />
+        <Text data="All Rights Reserved." />
+      </div>
     </div>
   );
 }
 
 export default Footer;
- 
